58637ef8c9e6caa5f1f6fa5b956d8755161c7324
aconkli9 <aconkli9@ford.com>
2022-04-07 13:31:15 -0400

    Add solution

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function displayPlan(){
+function displayPlan() {
   const step1 = "Collect underpants."
   const step2 = "???"
   const step3 = "Profit!"
+  const button = document.querySelector("button")
+
+  document.querySelector("#step-1").textContent = step1
+  document.querySelector("#step-2").textContent = step2
+  document.querySelector("#step-3").textContent = step3
 }
